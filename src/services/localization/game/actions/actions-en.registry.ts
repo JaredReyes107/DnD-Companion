75b295856a6a12663b4cfaad8a346be32786fc4a
Jared Reyes <jaredreyes236@gmail.com>
2026-06-26 09:53:44 -0600

    fix: add and correct imports

--- src/services/localization/game/actions/actions-en.registry.ts
+++ src/services/localization/game/actions/actions-en.registry.ts
@@ -3,11 +3,13 @@ import { GameLocalization } from "@/services/localization/game-localization.type
 import { barbarian_actions_en } from "../classes/barbarian/en-barbarian-actions";
 import { bard_actions_en } from "../classes/bard/en/bard-actions-en";
 import { cleric_actions_en } from "../classes/cleric/en/cleric-actions-en";
+import { druid_actions_en } from "../classes/druid/en/druid-actions-en";
 
 export const actions_en: GameLocalization = {
   actions: {
     ...barbarian_actions_en,
     ...bard_actions_en,
     ...cleric_actions_en,
+    ...druid_actions_en,
   },
 };
