92168a518b502785b520973b34180d89e25ef39e
Jared Reyes <jaredreyes236@gmail.com>
2026-08-20 15:05:06 -0500

    fix: missing and incorrect monk localization entries

--- src/services/localization/game/features/features-es.registry.ts
+++ src/services/localization/game/features/features-es.registry.ts
@@ -5,6 +5,7 @@ import { bard_features_es } from "../classes/bard/es/bard-features-es";
 import { cleric_features_es } from "../classes/cleric/es/cleric-features-es";
 import { druid_features_es } from "../classes/druid/es/druid-features-es";
 import { fighter_features_es } from "../classes/fighter/es/fighter-features-es";
+import { monk_features_es } from "../classes/monk/es/monk-features-es";
 import { paladin_features_es } from "../classes/paladin/es/paladin-features-es";
 
 export const features_es: GameLocalization = {
@@ -14,6 +15,7 @@ export const features_es: GameLocalization = {
     ...cleric_features_es,
     ...druid_features_es,
     ...fighter_features_es,
+    ...monk_features_es,
     ...paladin_features_es,
   },
 };
